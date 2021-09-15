1a60b04259b4eede6d684cfeec2a049e9eb9cafb
soni.kanishq <kanishqsoni@gmail.com>
2021-09-15 18:00:18 +0530

    Fourth Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 //HOC
-import DefaultHOC from "./HOC/default.hoc";
+import DefaultHOC from "./HOC/Default.HOC";
 import MovieHOC from "./HOC/Movie.HOC";
 
 //Pages
 import HomePage from "./pages/Home.page";
 import Movie from "./pages/Movie.page";
-
+import Plays from "./pages/Plays.page";
 
 // Import css files
 import "slick-carousel/slick/slick.css";
@@ -16,6 +16,7 @@ function App() {
     <>
      <DefaultHOC path="/" exact component={HomePage} />
      <MovieHOC path="/movie/:id" exact component={Movie} />
+     <DefaultHOC path="/plays" exact component={Plays} />
     </>
   );
 }
--- src/components/MovieHero/MovieHero.component.js
+++ src/components/MovieHero/MovieHero.component.js
@@ -16,8 +16,7 @@ const MovieHero = () => {
      alt="poster"
     />
    </div>
-
-   <div className="relative hidden lg:block" style={{height: "30rem"}}>
+   <div className="relative hidden lg:block" style={{height: "35rem"}}>
 
 <div className="absolute h-full w-full z-10"
 style={{ backgroundImage: "linear-gradient(90deg, rgb(34, 34, 34) 24.97%, rgb(34, 34, 34) 38.3%, rgba(34, 34, 34, 0.04) 97.47%, rgb(34, 34, 34) 100%)"}}
